2def6bdaf91788d5ad7e66da48586ba1fae7824c
KrishnTechK <Krishnaa.connect@gmail.com>
2026-07-12 13:51:24 +0530

    Implement RBAC authorization

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,9 +4,21 @@ const router = express.Router();
 const { login, getProfile } = require("../controllers/authController");
 
 const verifyToken = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
 
 router.post("/login", login);
 
 router.get("/profile", verifyToken, getProfile);
 
+router.get(
+  "/admin",
+  verifyToken,
+  roleMiddleware("Fleet Manager"),
+  (req, res) => {
+    res.json({
+      success: true,
+      message: "Fleet Manager Access Granted",
+    });
+  }
+);
 module.exports = router;
