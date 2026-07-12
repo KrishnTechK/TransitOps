8ece410a49976a7cc27f2af20152610df9729e4c
Vipin Gangwar <vipin63966345557@gmail.com>
2026-07-12 16:12:30 +0530

    final backend

---
+++ server/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+
+const vehicleRoutes = require("./routes/vehicleRoutes");
+const driverRoutes = require("./routes/driverRoutes");
+const tripRoutes = require("./routes/tripRoutes");
+const maintenanceRoutes = require("./routes/maintenanceRoutes");
+
+const notFound = require("./middleware/notFound");
+const errorHandler = require("./middleware/errorMiddleware");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Health Check
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Transport Management API is running...",
+  });
+});
+
+// API Routes
+app.use("/api/vehicles", vehicleRoutes);
+app.use("/api/drivers", driverRoutes);
+app.use("/api/trips", tripRoutes);
+app.use("/api/maintenance", maintenanceRoutes);
+
+// 404 Middleware
+app.use(notFound);
+
+// Global Error Middleware
+app.use(errorHandler);
+
+module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,28 @@
+// require("dotenv").config();
+
+// const app = require("./app");
+// const connectDB = require("./config/db");
+
+// // Connect Database
+// connectDB();
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server is running on http://localhost:${PORT}`);
+// });
+
+
+require("dotenv").config();
+
+const app = require("./app");
+const connectDB = require("./config/db");
+
+// Connect Database
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
